e9573d13e1a4e85715397d53162f07e42dc743b0
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2020-12-18 16:14:44 -0600

    now have buttons that log to the console and an empty object.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,5 +4,40 @@ $(readyNow);
 
 function readyNow() {
     console.log('jQuery ready.');
+
+    
+    $('#addition').on('click', add);
+    $('#subtraction').on('click', subtract);
+    $('#multiply').on('click', multiply);
+    $('#divide').on('click', divide);
+    $('#submit').on('click', submit);
+}
+
+let calculation = {
+    firstNumber: 0,
+    secondNumber: 0,
+    operator: 0
+}
+
+console.log(calculation);
+
+
+function add() {
+    console.log('Add');
+    
+}
+function subtract() {
+    console.log('subtract');
+    
+}
+function multiply() {
+    console.log('multiply');
+    
+}
+function divide() {
+    console.log('divide');
+}
+function submit() {
+    console.log('submit');
     
 }
